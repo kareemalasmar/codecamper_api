ba275359a8b77fc6b8d00c857c35d9dd30dc5f3d
KA <kareemalasmar@gmail.com>
2020-05-11 21:15:16 -0700

    role authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -28,8 +28,24 @@ exports.protect = asyncHandler(async (req, res, next) => {
     console.log(decoded);
 
     req.user = await User.findById(decoded.id);
+
     next();
   } catch (err) {
     return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
