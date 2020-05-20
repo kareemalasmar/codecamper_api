c121417f5a9df943a8890afab9f3f11d7f960d6f
KA <kareemalasmar@gmail.com>
2020-05-19 20:44:07 -0700

    XSS Protection and security headers

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const colors = require('colors');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -46,6 +48,12 @@ app.use(fileUpload());
 
 // Sanitize Data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
